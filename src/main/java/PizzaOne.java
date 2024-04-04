a8b92e16dd6cd61accef38cf9166db06ffa6dbe3
tshaker <164261635+tshaker@users.noreply.github.com>
2024-04-04 00:23:08 +0000

    add javadoc

--- src/main/java/PizzaOne.java
+++ src/main/java/PizzaOne.java
@@ -6,13 +6,18 @@
 
   CS A170
   Prof. Mayada Alani
-  Project 2
+  Project 2 Part One
 */
 
 import java.util.Scanner;
 
 public class PizzaOne {
 
+  /**
+   * Executes the pizza ordering program. 
+   *
+   * @param args the command line arguments 
+   */
   public static void main(String[] args) {
     // Scanner and Variables
     Scanner sc = new Scanner(System.in);
@@ -27,26 +32,23 @@ public class PizzaOne {
     char yesNo = ' ';
 
     final double DISCOUNT = 2.00;
-    final double TAX_RATE =  0.0795;
+    final double TAX_RATE = 0.0795;
     final double TOPPING_CHARGE = 1.25;
     double total = 0;
 
-    //NAME: user prompt
+    // NAME: user prompt
     System.out.println("Welcome to Tim and Kaylyn’s Pizzeria");
     System.out.print("Enter your first name: ");
     String name = sc.next();
-    //NAME: discount eligibility
-    if (name.equalsIgnoreCase("Kaylyn") || name.equalsIgnoreCase("Tim"))
-        {
-          discounted = true;
-          if(discounted)
-          {
-            System.out.println();
-            System.out.println("You have the same name as one of the owners, you are eligible for a $2.00 discount!");
-            total -= DISCOUNT;
-          }
-        }
-       
+    // NAME: discount eligibility
+    if (name.equalsIgnoreCase("Kaylyn") || name.equalsIgnoreCase("Tim")) {
+      discounted = true;
+      if (discounted) {
+        System.out.println();
+        System.out.println("You have the same name as one of the owners, you are eligible for a $2.00 discount!");
+        total -= DISCOUNT;
+      }
+    }
 
     // MENU: SIZE & COST
     System.out.println();
@@ -58,61 +60,51 @@ public class PizzaOne {
     System.out.println("    16\"     |  $16.99");
     System.out.println();
 
-    //SIZE: user prompt
+    // SIZE: user prompt
     System.out.println("What size pizza would you like?");
     System.out.print("10, 12, 14, or 16 (enter the number only): ");
     size = sc.nextInt();
-    //SIZE: pricing
-    if(size == 10)
-    {
+    // SIZE: pricing
+    if (size == 10) {
       total += 10.99;
-    }
-    else if (size == 12)
-    {
+    } else if (size == 12) {
       total += 12.99;
-    }
-    else if (size == 14)
-    {
+    } else if (size == 14) {
       total += 14.99;
-    }
-    else if (size == 16)
-    {
+    } else if (size == 16) {
       total += 16.99;
-    }
-    else
-    {
+    } else {
       System.out.println("Your input was not one of the choices, so a 12-inch pizza will be made.");
       total += 12.99;
       size = 12;
     }
 
-    
     System.out.println(); // line-break
 
-    //CRUST: user prompt
+    // CRUST: user prompt
     System.out.println("What type of crust do you want?");
     System.out.println("	(H)Hand-tossed, (T) Thin-crust, or (D) Deep-dish");
     System.out.print("enter (H, T, or D):");
     crust = sc.next().toUpperCase().charAt(0);
-    switch (crust) { 
+    switch (crust) {
       case 'H':
-      printCrust = "Hand-tossed crust";
+        printCrust = "Hand-tossed crust";
         break;
       case 'T':
-      printCrust = "Thin-crust";
+        printCrust = "Thin-crust";
         break;
       case 'D':
-      printCrust = "Deep-dish";
+        printCrust = "Deep-dish";
         break;
       default:
-      System.out.println("Your input was not one of the choices, so a Hand-tossed crust will be made.");
-      printCrust = "Hand-tossed crust";
+        System.out.println("Your input was not one of the choices, so a Hand-tossed crust will be made.");
+        printCrust = "Hand-tossed crust";
         break;
     }
 
     System.out.println(); // line-break
 
-    //TOPPINGS: user prompt
+    // TOPPINGS: user prompt
     System.out.println("All pizzas come with cheese.");
     System.out.println("Additional toppings are $1.25 each, choose from:");
     System.out.println("Pepperoni, Sausage, Onion, Mushroom");
@@ -123,7 +115,7 @@ public class PizzaOne {
       numOfToppings++;
       toppings += "Pepperoni ";
     }
-    
+
     System.out.print("	Do you want Sausage? (Y/N): ");
     yesNo = sc.next().toUpperCase().charAt(0);
     if (yesNo == 'Y') {
@@ -144,19 +136,18 @@ public class PizzaOne {
       numOfToppings++;
       toppings += "Mushroom ";
     }
-    //TOPPINGS: additional charge
+    // TOPPINGS: additional charge
     total += (numOfToppings * TOPPING_CHARGE);
 
-    System.out.println(); //line-break
+    System.out.println(); // line-break
 
     // Order RECEIPT
-    System.out.println("Your order is as follows:"); 
-    System.out.println(size + "-inch pizza"); //SIZE
-    System.out.println(printCrust); //CRUST
-    System.out.println(toppings); //TOPPINGS
+    System.out.println("Your order is as follows:");
+    System.out.println(size + "-inch pizza"); // SIZE
+    System.out.println(printCrust); // CRUST
+    System.out.println(toppings); // TOPPINGS
 
-    
-    //TOTAL PRICING
+    // TOTAL PRICING
     System.out.println();
     System.out.printf("The cost of your order is: $%.2f%n", total);
     System.out.printf("The tax is: $%.2f%n", (total * TAX_RATE));
